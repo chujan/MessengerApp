3a15669bcd3c2882618616f6a02022f61843d612
Chukwuemeka Jennifer <jennichuks9@gmail.com>
2021-08-05 15:33:17 +0100

    Initial Commit

---
+++ MessengerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessengerApp
+//
+//  Created by Chukwuemeka Jennifer on 05/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
